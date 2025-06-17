e174ac716bcd3c715e39fd6e6fc8e25b1bc621f0
dingrui <Bannirui@outlook.com>
2025-06-17 21:36:34 +0800

    源码调试

---
+++ my_exe.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+JDK_HOME=/home/dingrui/MyDev/code/cpp/jdk/build/linux-x86_64-server-slowdebug/jdk
+JAVAC=${JDK_HOME}/bin/javac
+JAVA=${JDK_HOME}/bin/java
+
+# 类(java不带.java后缀文件名)
+CLASS_NAME=$1
+if [ -z "$CLASS_NAME" ]; then
+  echo "Usage: $0 Hello"
+  exit 1
+fi
+
+# 源文件
+SRC_FILE_ABS_PATH=$(pwd)/my_test/${CLASS_NAME}.java
+
+echo "[*] Compiling $CLASS_NAME ..."
+$JAVAC "$SRC_FILE_ABS_PATH"
+
+if [ $? -ne 0 ]; then
+  echo "[!] Compilation failed."
+  exit 1
+fi
+
+echo "[*] Running $CLASS_NAME ..."
+$JAVA -cp my_test $CLASS_NAME
